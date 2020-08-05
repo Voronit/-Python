a2feb57fa30af1e0f478bfbed9d847d4a2cb37c7
Voronit <dima_kro2000@mail.ru>
2020-08-05 16:18:00 +0300

    123

--- Основы Python/2. Условные конструкции/Деление шоколада.py
+++ Основы Python/2. Условные конструкции/Деление шоколада.py
@@ -2,7 +2,7 @@ n = int(input ("Enter the length of the chocolate: "))
 m = int(input ("Enter the width of the chocolate: "))
 k = int(input ("How many slices do you want to take? "))
 
-if (k % n == 0) or (k % m == 0):
+if (k <= n * m) and ((k % n == 0) or (k % m == 0)):
   print("YES")
 else:
   print("NO")
